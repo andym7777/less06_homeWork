0ea3070a15788d0795a22c366d495635e4b1924e
andym7777 <37099541+andym7777@users.noreply.github.com>
2018-04-08 23:30:51 +0300

    Add files via upload

---
+++ Les06HomeWork/src/andym/StudentLogic.java
@@ -0,0 +1,17 @@
+package andym;
+
+public class StudentLogic {
+
+	public double getAvrMark(Student st) {
+		double avr = 0;
+
+		for (double d : st.getMarks()) {
+			avr = avr + d;
+		}
+
+		avr = avr / st.getCountOfMarks();
+
+		return avr;
+	}
+
+}
